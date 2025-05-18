a3cc64a74b965d9dd897391308715f0ef39aad44
Omar-EN-code <codeloccol.check4@gmail.com>
2025-05-18 07:54:37 +0200

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # Ce code permet de verifier si un mot se trouve dans un fichier
 echo ""
+#Lire le nom du fichier
 read file
 echo ""
+#Lire le mot a chercher
 read mot
 echo ""
+# Ferifier si le mot existe dans le fichier
 if  grep -q "$mot" "$file"; then
+# La phrase a afficher si le mot exixiste
 echo "La chaîne '$mot' a été trouvée dans $file"
 else
+# La phrase a afficher si le mot n'existe pas
 echo "La chaîne '$mot' n'a pas été trouvée dans $file."
 fi
