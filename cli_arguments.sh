4fc6abb39abc13df7213e2ba0218670de4b0a496
Omar-EN-code <codeloccol.check4@gmail.com>
2025-05-18 07:59:43 +0200

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Lire tous les arguments comme une chaine
 read -a args
+# Initialisation du numerotation des arguments
 k=1
+# Parcourrir et afficher les arguments
 for i in "${args[@]}"; do
 echo "Argument $k: $i"
 k=$(($k+1))
