1f3a0855fea53fada3420b22e3cf9e0d00ad546c
Omar-EN-code <codeloccol.check4@gmail.com>
2025-05-18 08:01:52 +0200

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Demande à l'utilisateur de saisir le nom du dossier
-read Myfolder
+read folder
 
 #recuperation des nombres des fichiers 
-count=$(ls "$Myfolder" | wc -l)
+count=$(ls -1 "$folder" | wc -l)
 
-# si le compte est bien fait 
+# si le compte contient du valeure
 if [ -z $count ]; then
 
   #Resultat
-  echo "Le dossier $Myfolder contient $count fichier(s)."
+  echo "Le dossier $folder contient $count fichier(s)."
 fi
